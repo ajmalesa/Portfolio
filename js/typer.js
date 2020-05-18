ea05f5222f05edcf575aee82f67edbce1f048242
Ajmal <ajmalesa@gmail.com>
2020-05-17 18:31:05 -0600

    Added message to check back later

--- js/typer.js
+++ js/typer.js
@@ -1,5 +1,5 @@
 var options = {
-    strings: ['Welcome to my website. My name is Ajmal. People call me AJ.'],
+    strings: ['Welcome to my website. My name is Ajmal. People call me AJ. Check back later for updates.'],
     typeSpeed: 60,
     showCursor: false,
   };
