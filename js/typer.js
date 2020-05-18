da03dd8ca757cbfae877476100ade6e97a00f495
Ajmal <ajmalesa@gmail.com>
2020-05-17 18:28:38 -0600

    Added typing animation functionality using typing.js && center content on smaller screens

---
+++ js/typer.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['Welcome to my website. My name is Ajmal. People call me AJ.'],
+    typeSpeed: 60,
+    showCursor: false,
+  };
+   
+var typed = new Typed('#dialog', options)
