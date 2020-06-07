94cb0c96a07666eb2745d43febd32beee12b3f25
Ajmal <ajmalesa@gmail.com>
2020-06-06 20:57:48 -0600

    Added popup that will let user know how to enable mouse again (esc key)

---
+++ js/controls.js
@@ -0,0 +1,5 @@
+document.addEventListener('keydown', function(event) {
+    if (event.code == "Escape") {
+        document.body.style.cursor = "inherit"
+    }
+});
--- js/typer.js
+++ js/typer.js
@@ -1,7 +1,26 @@
+// Set typeSpeed by what option the user selects
+let typeSpeedSetByUser = 80;
+function handleClick(speedSelected) {
+  document.getElementById("dialog").innerText = ""
+  var typed = new typed('dialog', options);
+  alert(speedSelected.value);
+  typeSpeedSetByUser = speedSelected.value;
+}
+
+// Set options for text that will be typed
 var options = {
-    strings: ['Welcome to my website. My name is Ajmal. People call me AJ. Check back later for updates.'],
-    typeSpeed: 60,
-    showCursor: false,
-  };
+  strings: [
+    "^1500 Welcome to my website." +
+    "^250 My name is Ajmal." +
+    "^250 People call me AJ." +
+    "^250 Check back later for updates.▼'"
+  ],
+  typeSpeed: typeSpeedSetByUser,
+  showCursor: false,
+  onComplete: (self) => {}
+};
    
-var typed = new Typed('#dialog', options)
+// Display text given in options on element with an ID of dialog 
+var typed = new Typed('#dialog', options);
+
+
