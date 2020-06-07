5ff1af0a3c51f8403fe8af6a7b46b2da0a3ae394
Ajmal <ajmalesa@gmail.com>
2020-06-06 21:08:33 -0600

    Grammatical error

--- js/typer.js
+++ js/typer.js
@@ -13,7 +13,7 @@ var options = {
     "^1500 Welcome to my website." +
     "^250 My name is Ajmal." +
     "^250 People call me AJ." +
-    "^250 Check back later for updates.▼'"
+    "^250 Check back later for updates.▼"
   ],
   typeSpeed: typeSpeedSetByUser,
   showCursor: false,
