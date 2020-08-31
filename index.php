ff0f7ebd341ec316d9b4d4540c005cba1347e56e
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-08-30 22:10:58 -0600

    Added favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Ajmal Esa's web projects. Peruse and contact me if you like what you see.">
     <title>Ajmal Esa</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
     <!-- UIkit CSS -->
     <link rel="stylesheet" href="css/uikit.min.css" />
