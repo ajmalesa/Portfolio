29d8dd786581bb8d40cf6d175c4fe7c4f4f0ab4c
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-08-31 00:41:46 -0600

    Mobile navbar also updates active anchor tag

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ document.getElementById('projects-button').addEventListener('click', function() 
     this.parentElement.classList.add('uk-active');
     document.getElementById('skills-button').parentElement.classList.remove('uk-active');
 })
+
+document.getElementById('skills-button-mobile').addEventListener('click', function() {
+    this.parentElement.classList.add('uk-active');
+    document.getElementById('projects-button-mobile').parentElement.classList.remove('uk-active');
+})
+
+document.getElementById('projects-button-mobile').addEventListener('click', function() {
+    this.parentElement.classList.add('uk-active');
+    document.getElementById('skills-button-mobile').parentElement.classList.remove('uk-active');
+})
