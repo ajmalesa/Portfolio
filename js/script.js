4ebbeeece5bb88d3126610cc08b8da98a934d115
Ajmal Esa <7179908+ajmalesa@users.noreply.github.com>
2020-09-01 18:09:37 -0600

    New section and show all/hide all skills function
    - JavaScript now handles toggle state of navbar for about section
    - This functionality was added to mobile and desktop
    - Added show all and hide all for skills section that expands
      and collapses all list items in accordions accordingly
    - Prevent default behavior of those buttons so page does not scroll

--- js/script.js
+++ js/script.js
@@ -1,20 +1,69 @@
+// Add active class on selected section and remove active class on all other desktop nav menu items 
 document.getElementById('skills-button').addEventListener('click', function() {
     this.parentElement.classList.add('uk-active');
     document.getElementById('projects-button').parentElement.classList.remove('uk-active');
-    
-})
-
+    document.getElementById('about-button').parentElement.classList.remove('uk-active');
+});
 document.getElementById('projects-button').addEventListener('click', function() {
     this.parentElement.classList.add('uk-active');
     document.getElementById('skills-button').parentElement.classList.remove('uk-active');
-})
+    document.getElementById('about-button').parentElement.classList.remove('uk-active');
+});
+document.getElementById('about-button').addEventListener('click', function() {
+    this.parentElement.classList.add('uk-active');
+    document.getElementById('skills-button').parentElement.classList.remove('uk-active');
+    document.getElementById('projects-button').parentElement.classList.remove('uk-active');
+});
 
+// Add active class on selected section and remove active class on all other mobile nav menu items 
 document.getElementById('skills-button-mobile').addEventListener('click', function() {
     this.parentElement.classList.add('uk-active');
     document.getElementById('projects-button-mobile').parentElement.classList.remove('uk-active');
-})
-
+    document.getElementById('about-button-mobile').parentElement.classList.remove('uk-active');
+});
 document.getElementById('projects-button-mobile').addEventListener('click', function() {
     this.parentElement.classList.add('uk-active');
     document.getElementById('skills-button-mobile').parentElement.classList.remove('uk-active');
-})
+    document.getElementById('about-button-mobile').parentElement.classList.remove('uk-active');
+});
+document.getElementById('about-button-mobile').addEventListener('click', function() {
+    this.parentElement.classList.add('uk-active');
+    document.getElementById('skills-button-mobile').parentElement.classList.remove('uk-active');
+    document.getElementById('projects-button-mobile').parentElement.classList.remove('uk-active');
+});
+
+// Expand all skills when show all button is clicked
+document.getElementById('show-all-button').addEventListener('click', function() {
+    // Go through each element that has a class of accordion
+    document.querySelectorAll('.accordion').forEach(
+        element => {
+            // Only going to toggle this accordion if it's children are closed
+            if (!element.children[0].classList.contains('uk-open')) {
+                // Use UIkit's toggle method to change state of accordion 
+                UIkit.accordion(element).toggle();
+            }
+        }
+    );
+});
+// Collapse all skills when hide all button is clicked
+document.getElementById('hide-all-button').addEventListener('click', function() {
+    // Go through each element that has a class of accordion
+    document.querySelectorAll('.accordion').forEach(
+        element => {
+            // Only going to toggle this accordion if it's children are already opened
+            if (element.children[0].classList.contains('uk-open')) {
+                // Use UIkit's toggle method to change state of accordion 
+                UIkit.accordion(element).toggle();
+            }
+        }
+    );
+});
+
+// Prevent the default action on show all and hide all buttons so page does not scroll on click
+document.getElementById("show-all-button").addEventListener("click", function(event){
+    event.preventDefault()
+});
+document.getElementById("hide-all-button").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
