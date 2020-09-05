b968a1fae1ee72eb429716f1c60aae72eb373438
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-09-05 03:28:56 -0600

    Add contact email to mail function with newlines

--- functions/contact-request.php
+++ functions/contact-request.php
@@ -3,6 +3,6 @@
     $contactEmail = $_POST["contact-email"];
     $contactMessage = $_POST["contact-message"];
 
-    mail("ajmalesa@gmail.com", "Contact", $contactMessage . "<br /> sender's email: " . $contactMessage);
+    mail("ajmalesa@gmail.com", "Contact", $contactMessage . "\r\n Sender's email: " . $contactEmail);
 
 ?>
