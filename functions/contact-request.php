b7e3f0fd948ae33b9ab38c8a39714bb55c478495
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-09-05 03:32:23 -0600

    Added one more line break in contact email body

--- functions/contact-request.php
+++ functions/contact-request.php
@@ -3,6 +3,6 @@
     $contactEmail = $_POST["contact-email"];
     $contactMessage = $_POST["contact-message"];
 
-    mail("ajmalesa@gmail.com", "Contact", $contactMessage . "\r\n Sender's email: " . $contactEmail);
+    mail("ajmalesa@gmail.com", "Contact", $contactMessage . "\r\n\r\n Sender's email: " . $contactEmail);
 
 ?>
