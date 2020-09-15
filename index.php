7708fd47b5adbf136624ef4ed3db88c69b0d48d0
AJEsa <7179908+ajmalesa@users.noreply.github.com>
2020-09-15 16:29:46 -0600

    Lighthouse Best Practices Added rel=noreferrer
    - Added to latest commit link as suggested by Lighthouse for
      cross-origin domains

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
                 <div id="commit-section" class="uk-width-1-2@l">
                     <div uk-scrollspy="cls: uk-animation-scale-up uk-transform-origin-top-right; repeat: true">
                         <span id="latest-commit-display"></span>
-                        <a href="#" target="_blank" id="latest-commit-link">    
+                        <a href="#" target="_blank" id="latest-commit-link" rel="noreferrer">    
                             <div id="latest-commit-project" class="uk-text-capitalize"></div>
                             <div id="latest-commit-message"></div>
                             <div id="latest-commit-time"></div>
