a42579736b60974eb12f451dfc3f78116372f598
Ray Ren <7179908+ajmalesa@users.noreply.github.com>
2022-02-08 03:06:20 -0700

    Changed introduction paragraph

--- index.php
+++ index.php
@@ -78,8 +78,8 @@
                 <div class="uk-width-1-2@l">
                     <p>
                         <span class="fade-first-sentence">I am a web developer with a passion for all things tech.</span> 
-                        <span class="fade-second-sentence">I started in high school.</span>
-                        <span class="fade-third-sentence">Now it has evolved into my livelihood.</span> 
+                        <span class="fade-second-sentence">It started with writing automation scripts in high school.</span>
+                        <span class="fade-third-sentence">Now I make websites that look and work great.</span> 
                     </p>
                 </div>
                 <div id="commit-section" class="uk-width-1-2@l">
